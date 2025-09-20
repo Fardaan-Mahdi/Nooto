8cf8e9adc8c224eceb608c95fd78e84dd2880dc1
Fardaan Mahdi <codingmonk501@gmail.com>
2025-09-20 18:35:55 +0530

    Fix search functionality to work when deleting text

---
+++ notes-app-react/src/components/NavBar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { FaSquarePlus } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
+const NavBar = ({ searchText, handelSearchText }) => {
+  const handleLogoClick = () => {
+    window.location.reload(); // Refreshes the entire page
+  };
+  return (
+    <nav className="navbar bg-body-tertiary py-50" style={{ padding: "20px" }}>
+      <div className="container d-flex justify-content-around">
+        <div
+          className="navbar-brand"
+          style={{ cursor: "pointer" }}
+          onClick={handleLogoClick}
+        >
+          <h4 style={{ fontWeight: "bold" }}>Notoo</h4>
+        </div>
+        <div className="d-flex">
+          <div
+            className="input-group input-group-sm"
+            style={{ width: "500px", height: "40px" }}
+          >
+            <input
+              className="form-control"
+              placeholder="Search"
+              value={searchText}
+              onChange={(e) => {
+                handelSearchText(e.target.value);
+              }}
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </div>
+        </div>
+
+        <Link to="/add-note" style={{ textDecoration: "none" }}>
+          <button className="btn btn-outline-primary btn-md" type="button">
+            <FaSquarePlus className="me-2 fs-6" /> Add Notes
+          </button>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
